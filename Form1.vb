e03b1b6bbcd45742802e2b1dbb0d6a049ee1e4a1
Kenneth <kennethamboya101@gmail.com>
2024-11-09 15:47:34 +0800

    revert again AAAAA

--- Form1.vb
+++ Form1.vb
@@ -1,35 +1,3 @@
 ﻿Public Class mainForm
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
-
-    Private Sub Label10_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Label24_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Label25_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub FlowLayoutPanel1_Paint(sender As Object, e As PaintEventArgs)
-
-    End Sub
-
-    Private Sub ProgressBar4_Click(sender As Object, e As EventArgs)
-
-    End Sub
 End Class
